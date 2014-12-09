9e7875076501e0c3a08171f8fd0c0afdafe66d72
Muini <corentin.flach@gmail.com>
2014-12-09 17:04:45 +0100

    Petite mise en page & fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :url_img, presence: true
-  validates :url_film, presence: true
+  validates :url_img, presence: true, format: /.(png|jpg)\Z/
+  validates :url_film, presence: true, format: /vodkaster\.com\/films/i
   validates :film_name, presence: true
 
   default_scope -> { order('created_at DESC') }
