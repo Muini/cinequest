0cf067ae3ff61578e447a1c30e925c2d4ae20707
Muini <corentin.flach@gmail.com>
2014-12-10 18:35:43 +0100

    Regex Pseudo + DB online

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   before_save { self.email = email.downcase }
-  validates :pseudo, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :pseudo, presence: true, uniqueness: true, format: /\A[a-zA-Z0-9_]{3,25}\z/, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, length: { maximum: 255 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6, maximum: 25 }
