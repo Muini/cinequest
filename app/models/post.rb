33aabf573aabc29cbabacbc8ef718302c36d825b
Muini <corentin.flach@gmail.com>
2014-12-12 10:20:06 +0100

    JS Forms Check

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   before_save { self.difficulty = difficulty.to_i }
   validates :url_img, presence: true, format: /.(png|jpg)\Z/
   validates :url_film, presence: true, format: /vodkaster\.com\/films/i
-  validates :film_name, presence: true
+  validates :film_name, presence: true, length: { minimum: 1, maximum: 255 }
   validates :clue, length: { maximum: 260 }
 
   default_scope -> { order('created_at DESC') }
