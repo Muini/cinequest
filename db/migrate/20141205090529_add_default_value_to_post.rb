277eca69368d9517a3d96f8ec48c3cce4612a3cd
Muini <corentin.flach@gmail.com>
2014-12-21 22:08:11 +0100

    DB fix

--- db/migrate/20141205090529_add_default_value_to_post.rb
+++ db/migrate/20141205090529_add_default_value_to_post.rb
@@ -3,7 +3,7 @@ class AddDefaultValueToPost < ActiveRecord::Migration
     change_column :posts, :found, :boolean, :default => false, :null => false
     change_column :posts, :hardlevel, :integer
     #change_column :posts, :hardlevel, 'integer USING CAST("hardlevel" AS integer)'
-    change_column :posts, :hardlevel, :integer, :default => 0  
+    #change_column :posts, :hardlevel, :integer, :default => 0  
     rename_column :posts, :user_id_id, :user_id
   end
 end
